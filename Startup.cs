9f3e334b22e27630ed94168095890eaa38b642b9
Aneena Shaji <ashaji@suyati.com>
2022-03-09 13:34:46 +0530

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(studentmanagementsystem.Startup))]
+namespace studentmanagementsystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
